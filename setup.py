938f2e296c2f7a31e34a214f8770dd54cce563ab
gsec <o0v0o.ix@gmail.com>
2018-09-09 17:08:18 +0200

    testing RTD docs

--- setup.py
+++ setup.py
@@ -148,10 +148,6 @@ setup_args = {
     'packages': find_packages(),
     'zip_safe': False,
     'cmdclass': {
-        'build_py': js_prerelease(build_py),
-        'egg_info': js_prerelease(egg_info),
-        'sdist': js_prerelease(sdist, strict=True),
-        'jsdeps': NPM,
     },
 
     'author': 'Maarten A. Breddels',
